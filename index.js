37ba7165dad58aeadb20ec9ed663a40e2588de4a
Patrick Kim <pkim050@gmail.com>
2019-05-23 20:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const var1 = 31;
 const var2 = 2;
 const multiply = var1 * var2;
 const random = Math.floor(Math.random() * 10) + 2;
-const mod = 7 % 18;
-const max = [0, 2, 20];
+const mod = 18 % 7;
+const max = Number.max([0, 2, 20]);
