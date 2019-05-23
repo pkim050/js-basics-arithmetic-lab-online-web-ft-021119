dbba10a178823331d367fe045429c8e016ddaae5
Patrick Kim <pkim050@gmail.com>
2019-05-23 20:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here
+const var1 = 31;
+const var2 = 2;
+const multiply = var1 * var2;
+const random = Math.ceiling(Math.random() * 10);
+const mod = 7 % 18;
+const max = [0, 2, 20];
